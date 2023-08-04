164b5e044b55e79cacd04173d263cc5965915088
Timo Prüße <chroma91@gmail.com>
2023-08-05 01:10:57 +0200

    use smaller bg image for mobile devices

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,8 +10,16 @@
 	section {
 		background-image: url('/images/bg_flowers.jpg');
 
-		@supports (background-image: url('/images/bg_flowers.webp')) {
-			background-image: url('/images/bg_flowers.webp');
+		@supports (background-image: url('/images/bg_flowers_portrait.webp')) {
+			background-image: url('/images/bg_flowers_portrait.webp');
+		}
+
+		@media screen and (min-width: 640px) {
+			background-image: url('/images/bg_flowers.jpg');
+
+			@supports (background-image: url('/images/bg_flowers.webp')) {
+				background-image: url('/images/bg_flowers.webp');
+			}
 		}
 	}
 </style>

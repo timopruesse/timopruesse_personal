70719fddb1e5392427af8422fc58559db2e8bb88
Timo Prüße <chroma91@gmail.com>
2024-04-13 00:51:03 +0200

    adjustments

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-    theme: {
-        extend: {
-            colors: {
-                'sky-blue': '#a8c5d0',
-            }
-        }
-    },
+	theme: {
+		extend: {
+			colors: {
+				'sky-blue': '#a8c5d0'
+			}
+		}
+	},
 
-    plugins: []
+	plugins: []
 };

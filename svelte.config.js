9872190d826d5674179104129c73c8c016e8c90a
Timo Prüße <chroma91@gmail.com>
2024-08-29 22:02:07 +0200

    chore: update config files

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,25 @@
-import adapter from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
-		preprocess({
-			postcss: true
+		sveltePreprocess({
+			postcss: true,
+			sourceMap: true
 		})
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: vercel({
+			maxDuration: 300
+		})
+	},
+
+	compilerOptions: {
+		enableSourcemap: true
 	}
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
+import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
 
-const config: UserConfig = {
-	plugins: [sveltekit()]
-};
+/** @type {import('vite').UserConfig} */
+const config = defineConfig({
+	server: {
+		https: {},
+		proxy: {}
+	},
+	plugins: [sveltekit()],
+	build: {
+		sourcemap: true,
+		rollupOptions: {
+			output: {
+				compact: true,
+				manualChunks: undefined
+			},
+			treeshake: 'smallest'
+		}
+	}
+});
 
 export default config;
